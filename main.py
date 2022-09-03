087fa8a6b390f78663d9eaecc1159e4ea11ab50f
SaintClever <parchment@gmx.com>
2022-09-03 16:44:54 -0400

    add flask_restful

--- main.py
+++ main.py
@@ -1,36 +1,41 @@
 from flask import Flask, jsonify
 from bs4 import BeautifulSoup
+from flask_restful import Resource, Api
 import requests
 
 
 app = Flask(__name__)
+api = Api(app)
 url = 'https://en.wikipedia.org/wiki/'
+# @app.route('/<string:user_query>.wiki-search.com')
 
+class Wiki(Resource):
+    def get(self, user_query):
+        response = requests.get(url + user_query)
+        doc = BeautifulSoup(response.text, 'html.parser')
+        div = doc.find('div', class_='mw-parser-output')
+        li = div.find_all('li')
 
-@app.route('/<string:user_query>.wiki-search.com')
-def index(user_query):
-    response = requests.get(url + user_query)
-    doc = BeautifulSoup(response.text, 'html.parser')
-    div = doc.find('div', class_='mw-parser-output')
-    li = div.find_all('li')
+        # loop through list
+        links = []
+        for link in li:
+            if user_query.capitalize() in str(link.text):
+                element = link.find(href=True)
+                try:
+                    element_href = element['href']
+                    links.append(url + element_href[element_href.rfind('/') + 1:])
+                except TypeError:
+                    pass
+
+        # decide whether to return links or a link
+        if links != []:
+            return jsonify({'links': [links]})
+        else:
+            return jsonify({'links': [url + user_query]})
     
 
-    links = []
-    for link in li:
-        if user_query.capitalize() in str(link.text):
-            element = link.find(href=True)
-            try:
-                element_href = element['href']
-                links.append(url + element_href[element_href.rfind('/') + 1:])
-            except TypeError:
-                pass
+api.add_resource(Wiki, '/<string:user_query>.wiki-search.com')
 
- 
-    if links != []:
-        return jsonify({'links': [links]})
-    else:
-        return jsonify({'links': [url + user_query]})
-    
 
 if __name__ == '__main__':
     app.run(debug=True)
