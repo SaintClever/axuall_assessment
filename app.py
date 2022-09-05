29118cc66c0c5d9e10d713d69eb6230c74d47415
SaintClever <parchment@gmx.com>
2022-09-05 08:24:53 -0400

    show description return value via __annotations__

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify
 from bs4 import BeautifulSoup
 from flask_restful import Resource, Api
 import requests
-
+import json
 
 app = Flask(__name__)
 api = Api(app)
@@ -11,7 +11,7 @@ url = 'https://en.wikipedia.org/wiki/'
 
 
 class Wiki(Resource):
-    def get(self, user_query):
+    def get(self, user_query) -> 'json':
         '''REATE REQUEST'''
         response = requests.get(url + user_query)
         doc = BeautifulSoup(response.text, 'html.parser')
@@ -26,9 +26,9 @@ class Wiki(Resource):
         links = []
         for link in li:
             if user_query.capitalize() in str(link.text):
-                element = link.find(href=True)
+                # element = link.find(href=True)
                 # if element:
-                if (element := link.find(href=True)):
+                if element := link.find(href=True):
                     element_href = element['href']
                     links.append(url + element_href[element_href.rfind('/') + 1:]) # REMOVE /wiki/
 
@@ -42,7 +42,7 @@ class Wiki(Resource):
 
 # RESOURCE API's
 api.add_resource(Wiki, '/<string:user_query>.wiki-search.com')
-
+print(Wiki.get.__annotations__)
 
 if __name__ == '__main__':
     app.run(debug=True)
